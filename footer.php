0af5f07fd339789662ffd5cc2da68bbb33053be5
Timur <105748461+1Jester1@users.noreply.github.com>
2024-05-21 08:24:43 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+  <footer class="container-footer">
+    <div class="footer">
+      <div>
+        2024
+      </div>
+      <div>
+        CopyRight
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
